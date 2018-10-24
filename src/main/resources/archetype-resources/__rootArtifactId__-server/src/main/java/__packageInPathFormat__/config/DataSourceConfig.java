700ffa494128e2ef6f49ccd180e447a9bf24f799
2118089226@qq.com <2118089226@qq.com>
2018-10-24 15:43:30 +0800

    代码修改

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-server/src/main/java/__packageInPathFormat__/config/DataSourceConfig.java
@@ -0,0 +1,53 @@
+package ${groupId}.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: lindj
+ * @date: 2018/5/16 17:06
+ * @description:
+ */
+@Configuration
+public class DataSourceConfig {
+
+    @Bean(name = "dataSource_druid",initMethod = "init", destroyMethod = "close")
+    @ConfigurationProperties(prefix="spring.datasource")
+    public DruidDataSource dataSource() {
+        DruidDataSource datasource = new DruidDataSource();
+        return datasource;
+    }
+
+    /**
+     * 注册ServletRegistrationBean
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean registrationBean() {
+        ServletRegistrationBean bean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+        /** 初始化参数配置，initParams**/
+        return bean;
+    }
+    /**
+     * 注册FilterRegistrationBean
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean druidStatFilter() {
+        FilterRegistrationBean bean = new FilterRegistrationBean(new WebStatFilter());
+        //添加过滤规则.
+        bean.addUrlPatterns("/*");
+        //添加不需要忽略的格式信息.
+        bean.addInitParameter("exclusions","*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return bean;
+    }
+
+
+
+}
