e4d9e2a73dadd520bc8e64279c2b9b413e6bc550
2118089226@qq.com <2118089226@qq.com>
2018-12-10 16:41:40 +0800

    代码修改

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-server/src/main/java/__packageInPathFormat__/config/ExecutorConfig.java
@@ -0,0 +1,51 @@
+package ${groupId}.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @author: lindj
+ * @date: 2018/12/10
+ * @description:
+ */
+@Configuration
+@EnableAsync
+public class ExecutorConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(ExecutorConfig.class);
+
+    @Value("${spring.executor.thread.corePoolSize}")
+    private int corePoolSize;
+    @Value("${spring.executor.thread.maxPoolSize}")
+    private int maxPoolSize;
+    @Value("${spring.executor.thread.queueCapacity}")
+    private int queueCapacity;
+    @Value("${spring.executor.thread.name.prefix}")
+    private String namePrefix;
+
+    @Bean(name = "asyncExecutorService")
+    public Executor asyncServiceExecutor() {
+        logger.info("threadpool start init");
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        //配置核心线程数
+        executor.setCorePoolSize(corePoolSize);
+        //配置最大线程数
+        executor.setMaxPoolSize(maxPoolSize);
+        //配置队列大小
+        executor.setQueueCapacity(queueCapacity);
+        //配置线程池中的线程的名称前缀
+        executor.setThreadNamePrefix(namePrefix);
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        //执行初始化
+        executor.initialize();
+        return executor;
+    }
+}
--- src/main/resources/archetype-resources/scripts/docker_republish.sh
+++ src/main/resources/archetype-resources/scripts/docker_republish.sh
@@ -6,13 +6,6 @@ if [ -z $port ]
   then port=23500
 fi
 
-if [ -z $dbp ] || [ -z $dbu ] || [ -z $dbp ] || [ -z $dbi ]
-
-  then
-     echo "dbport or dbuser or dbpwd or dbip  is null"
-     exit 1
-fi
-
 echo "container is stoping and removing"
 
 docker ps -a | grep -E "${service_name}" | awk '{print $1}' | xargs docker stop
@@ -28,11 +21,6 @@ mvn package docker:build
 echo "build success and container is starting"
 
 docker run -p ${port}:${port} \
-#       --env DB_PORT=${dbport} \
-#       --env USER=${dbuser} \
-#       --env PASSWORD=${dbpwd} \
-#       --env IP=${dbip} \
        --env SERVER_PORT=${port} \
-#       -v /images:/images \
        -v /data/servers/logs/${service_name}/:/data/servers/logs/${service_name} \
        -t ${application_name}/${service_name}:${version}
