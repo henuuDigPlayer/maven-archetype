1a467753565838ff1c9d39aa883b68b44080f3e2
2118089226@qq.com <2118089226@qq.com>
2019-03-01 14:28:17 +0800

    代码修改

--- src/main/resources/archetype-resources/__rootArtifactId__-server/src/main/java/__packageInPathFormat__/config/ExecutorConfig.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-server/src/main/java/__packageInPathFormat__/config/ExecutorConfig.java
@@ -1,15 +1,15 @@
 package ${groupId}.config;
-
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author: lindj
@@ -17,35 +17,20 @@ import java.util.concurrent.ThreadPoolExecutor;
  * @description:
  */
 @Configuration
-@EnableAsync
 public class ExecutorConfig {
 
     private static final Logger logger = LoggerFactory.getLogger(ExecutorConfig.class);
 
-    @Value("${spring.executor.thread.corePoolSize}")
-    private int corePoolSize;
-    @Value("${spring.executor.thread.maxPoolSize}")
-    private int maxPoolSize;
-    @Value("${spring.executor.thread.queueCapacity}")
-    private int queueCapacity;
-    @Value("${spring.executor.thread.name.prefix}")
-    private String namePrefix;
-
-    @Bean(name = "executorService")
-    public Executor asyncServiceExecutor() {
-        logger.info("threadpool start init");
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        //配置核心线程数
-        executor.setCorePoolSize(corePoolSize);
-        //配置最大线程数
-        executor.setMaxPoolSize(maxPoolSize);
-        //配置队列大小
-        executor.setQueueCapacity(queueCapacity);
-        //配置线程池中的线程的名称前缀
-        executor.setThreadNamePrefix(namePrefix);
-        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
-        //执行初始化
-        executor.initialize();
-        return executor;
+    @Bean("executorService")
+    public ExecutorService getExecutorService() {
+        ThreadFactory threadFactory =
+                new ThreadFactoryBuilder().setNameFormat("thread-pool-%d").build();
+        return new ThreadPoolExecutor(5,
+                10,
+                0L,
+                TimeUnit.SECONDS,
+                new LinkedBlockingDeque<>(),
+                threadFactory,
+                new ThreadPoolExecutor.AbortPolicy());
     }
 }
